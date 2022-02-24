0df6e59d5b3b3a73c1da3338b745b8ca3a77de98
coinlordd <35095979+coinlordd@users.noreply.github.com>
2022-02-24 22:43:24 +0100

    Update index.tsx

--- example/src/index.tsx
+++ example/src/index.tsx
@@ -5,7 +5,7 @@ import Synchronizer from './lib/synchronizer/instance'
 import App from './App'
 
 // Make sure the SynchronizerProvider is injected below the web3 context provider.
-// It has its own built-in store, so you don't have to combine reducers.
+// It has its own built-in store, so you won't have to deal with combining reducers.
 ReactDOM.render(
   <React.StrictMode>
     <Synchronizer>
